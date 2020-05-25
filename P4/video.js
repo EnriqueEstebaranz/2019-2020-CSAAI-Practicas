d1bb269a959b93cdabbd461ee6d267a56a80e045
enrique <enriqueestebaranzredondo@hotmail.com>
2020-05-25 13:57:28 +0200

    añadimos css con algunmos areglos del tamaño de pantalla de los videos

--- P4/video.js
+++ P4/video.js
@@ -2,8 +2,8 @@ console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
 const video_selecionado = document.getElementById("video_selecionado")
-video_selecionado.width=600;  //-- Tamaño de la pantalla de video
-video_selecionado.height=300;
+video_selecionado.width=520;  //-- Tamaño de la pantalla de video
+video_selecionado.height=290;
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
@@ -11,24 +11,24 @@ video_selecionado.poster="video.gif";
 
 //----- Obtener elemento de video y configurarlo
 const video1 = document.getElementById("video1")
-video1.width=200;  //-- Tamaño de la pantalla de video
-video1.height=100;
+video1.width=160;  //-- Tamaño de la pantalla de video
+video1.height=90;
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
 
 const video2 = document.getElementById("video2")
-video2.width=200;  //-- Tamaño de la pantalla de video
-video2.height=100;
+video2.width=160;  //-- Tamaño de la pantalla de video
+video2.height=90;
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
 
 const video3 = document.getElementById("video3")
-video3.width=200;  //-- Tamaño de la pantalla de video
-video3.height=100;
+video3.width=160;  //-- Tamaño de la pantalla de video
+video3.height=90;
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
